1c6dcda1e910ffbf1507c37ebc329cd33e10267c
sempercuriosus <vespers-garages.0n@icloud.com>
2023-10-14 08:47:24 -0400

    small refactor and needed to change the connection param. was able to get data returned to cli

--- db/scripts/Department.js
+++ db/scripts/Department.js
@@ -1,37 +1,43 @@
 
 // establish connection
-const connection = require("./db/connection");
+const connection = require("../connection");
 
 /**
- * @name 
- * @classdesc 
+ * @name Department
+ * @classdesc contains a constructor and several functions related to the Department
+ * 
+ * One can View, Update, Add new departments
  */
 class Department {
     /**
     * Property List for Department Class
-    * @property {} 
+    * @property {} connection is the connection established with the target database.
     * @property {} 
     */
     constructor () {
-        this.connection = connection.createConnection();
+        this.connection = connection;
     };
 
     // Function List in Department Class
     //
 
     /**
-     * @name viewDepartments
-     * @param {}  - 
+     * @name view
      * @returns a Promise Object containing ALL current departments
     */
-    viewDepartments () {
-        console.info("[ viewDepartments ] : called");
+    view () {
+        console.info("[ view ] : called");
 
         // the point is to return a Promise object, from which, data can be extrapolated, but here we return a Promise back to the index.js for manipulation.
-        const query = "SELECT * FROM departments";
+        const query = `
+        SELECT id
+            , name
+        FROM department
+        ;
+        `;
         return this.connection.promise().query(query);
 
-    }; //  [ end : viewDepartments ]
+    }; //  [ end : view ]
 
 
     /**
@@ -44,4 +50,5 @@ class Department {
 
 
 // export functions
-module.exports = Department;
+// **MUST** instantiate a new instance of the class on export... this will prevent the need to do this at **every** point in the program that Department is needed.
+module.exports = new Department(connection);
