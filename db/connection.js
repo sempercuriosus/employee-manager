f432789f733ea03953c10c070435d02c3d94d640
sempercuriosus <vespers-garages.0n@icloud.com>
2023-10-14 09:06:29 -0400

    added a new line before connection log.

--- db/connection.js
+++ db/connection.js
@@ -14,18 +14,22 @@ const connection_pool = mysql.createPool({
 });
 
 connection_pool.on("acquire", () => {
+    console.log("");
     console.info("You have connected to the [ " + defaultDB + " ] database!");
 });
 
 connection_pool.on("release", () => {
+    console.log("");
     console.info("Connection released.");
 });
 
 connection_pool.on("close", () => {
+    console.log("");
     console.info("Connection closed.");
 });
 
 connection_pool.on("error", (error) => {
+    console.log("");
     console.error("There was an error with the database connection.", "", "See the details");
     console.error(error);
 });
