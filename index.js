9b172680a16b13338856f5a54693539fb598c22b
sempercuriosus <vespers-garages.0n@icloud.com>
2023-10-15 19:10:05 -0400

    refactor comments done

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ function init () {
 /**
  * Renders the logo
  * @name displayLogo
- * @param {string} logoText is the text that you want to generate.
 */
 function displayLogo (logoText) {
     const renderedLogo = logo({ name: logoText, description: logoDescription, font: "Soft", borderColor: "bold-black", logoColor: "blue", textColor: "white" }).render();
@@ -38,7 +37,23 @@ function displayLogo (logoText) {
 
 /**
  * Starts the application, displying the menu options to the user such that they can complete the actions
- * @name mainMenu
+ * @name loadMainMenu
+ * 
+ * Current Options
+ * EMPLOYEE
+ * - View
+ * - Add
+ * - Update Role
+ * 
+ * ROLE
+ * - View
+ * - Add
+ * 
+ * DEPARTMENT
+ * - View
+ * - Add
+ * 
+ * Exit
 */
 function loadMainMenu () {
     inq
@@ -133,11 +148,13 @@ function loadMainMenu () {
         });
 }; //  [ end : mainMenu ]
 
+
+
 // #region Complete Action List
-//
-/*
- * This is a listing of the functions that are used in the application, breaking them out of the inquirer prompt flow
-*/
+
+
+
+
 
 // #region Departments
 //
@@ -147,39 +164,46 @@ function loadMainMenu () {
 
 
 
-
 /**
  * @name viewDepartments 
- * @returns Department Table Values
+ * @description Shows the current list of Departments
 */
 function viewDepartments () {
+    // getting the list of depts
     Department.view()
         .then(([ resData ]) => {
             let departments = resData;
             console.log("");
+            // table showing result-set
             console.table(departments);
         })
+        // return to menu
         .then(() => loadMainMenu());
-
 }; //  [ end : viewDepartment ]
+
+
 
 /**
  * @name addDepartment
- * @description Asks for the new Department's name, attempts to update the database, and the loads the new changes
+ * @description Adds a new Department to the database
+ * 
+ * Asks for:
+ * - Department Name
 */
 function addDepartment () {
     inq
-        // ask new department
+        // ask new department name
         .prompt([
             {
                 name: "name"
                 , message: "Provide the new Department name: "
             }
         ])
-        // insert
+        // getting the new name to use 
         .then(resData => {
             let departmentName = resData.name;
 
+            // Adding to the db
             Department.add(departmentName)
                 // confirmation message
                 .then(() => console.log("Added the Department:  " + departmentName), "")
@@ -199,6 +223,10 @@ function addDepartment () {
 // #endregion Departments
 
 
+
+
+
+
 // #region Roles
 //
 /*
@@ -207,22 +235,20 @@ function addDepartment () {
 
 
 
-
-
-
-
 /**
  * @name viewRoles
- * @param {} 
- * @returns Role Table Values
+ * @description Shows the current list of Roles
 */
 function viewRoles () {
+    // getting the list of depts
     Role.view()
         .then(([ resData ]) => {
             let roles = resData;
             console.log("");
+            // table showing result-set
             console.table(roles);
         })
+        // return to menu
         .then(() => loadMainMenu());
 
 }; //  [ end : viewRoles ]
@@ -231,7 +257,12 @@ function viewRoles () {
 
 /**
  * @name addRole
- * @description Asks for the new Role's name, attempts to update the database, and the loads the new changes
+ * @description Adds a new Role to the database
+ * 
+ * Asks for:
+ * - Role Name
+ * - Salary
+ * - Department Name
 */
 function addRole () {
     Department.view()
@@ -239,13 +270,14 @@ function addRole () {
             // this is here instead of destructuring, just to remind me this is a thing too.
             let departments = resData[ 0 ];
 
-            // get a map of the existing departments to select from
+            // mapping the departments for selection
+            // map fn values from result-set alias names
             const departmentList = departments.map(({ Department_ID, Department_Name }) => ({
                 name: Department_Name
                 , value: Department_ID
             }));
             inq
-                // ask role name
+                // ask role name, salary, department
                 .prompt([ {
                     name: "name"
                     , message: "Provide the new Role Name: "
@@ -261,15 +293,18 @@ function addRole () {
                     , choices: departmentList
                 }
                 ])
+                // passing along inq values
                 .then(roleData => {
                     let { name, salary, department } = roleData;
 
+                    // insert to db
                     Role.add(name, salary, department)
                         .then(() => {
                             console.log("Added the Role: " + name);
                             console.log("With the Salary: " + salary);
                             console.log("To the Department: " + department);
                         })
+                        // return to menu
                         .then(() => loadMainMenu());
                 });
         });
@@ -280,6 +315,10 @@ function addRole () {
 
 //
 // #endregion Roles
+
+
+
+
 
 
 // #region Employee
@@ -294,27 +333,29 @@ function addRole () {
 
 /**
  * @name viewEmployees
- * @description Shows a formatted table view of the current Employees
+ * @description Shows the current list of Employees
 */
 function viewEmployees () {
     Employee.view()
-        // destructure the results
+        // getting list of emp
         .then(([ resData ]) => {
-
             const employees = resData;
             console.log("");
+            // table showing result-set
             console.table(employees);
         })
+        // return to menu
         .then(() => loadMainMenu());
 
 }; //  [ end : viewEmployees ]
+
 
 
 /**
  * @name addEmployee
  * @description Adds a new Employee 
  * 
- * Asks for the following:
+ * Asks for:
  * - First Name
  * - Last Name 
  * - Role
@@ -324,7 +365,7 @@ function viewEmployees () {
  * with a confirmation that the Employee was added.
 */
 function addEmployee () {
-    // get the new logrmation on the employee
+    // get the new information on the employee
     // first and last name
     inq
         .prompt([ {
@@ -337,14 +378,15 @@ function addEmployee () {
         }
         ])
         .then(resData => {
+            // saving first and last for insert
             const firstName = resData.first_name;
             const lastName = resData.last_name;
 
-            // role of employee
             // get the list of roles
             Role.view()
                 .then(([ resData ]) => {
                     let roles = resData;
+                    // map fn values from result-set alias names
                     const roleList = roles.map(({ Role_ID, Role_Name }) => ({
                         "name": Role_Name
                         , "value": Role_ID
@@ -366,6 +408,7 @@ function addEmployee () {
                             Employee.listManagers()
                                 .then(([ resData ]) => {
                                     let employees = resData;
+                                    // map fn values from result-set alias names
                                     const managerList = employees.map(({ Employee_ID, First_Name, Last_Name, Employee_Role }) => ({
                                         "name": First_Name + " " + Last_Name + " - " + Employee_Role
                                         , "value": Employee_ID
@@ -389,6 +432,7 @@ function addEmployee () {
                                         .then((resData) => {
                                             const managerID = resData.manager;
 
+                                            // insert new emp
                                             Employee.add(firstName, lastName, roleId, managerID);
                                         })
                                         .then(() => {
@@ -410,8 +454,6 @@ function addEmployee () {
  * - Lists Employees to select from as the update target
  * - Lists Roles for the new role
  * Confirms the update by logging
- * @param {} - 
- * @returns - 
 */
 function updateRole () {
     console.log("[ updateRole ] : called");
@@ -422,6 +464,7 @@ function updateRole () {
 
             let employees = resData;
             // map the employees
+            // map fn values from result-set alias names
             const employeeList = employees.map(({ Employee_ID, First_Name, Last_Name, Employee_Role }) => ({
                 "name": First_Name + " " + Last_Name + " - " + Employee_Role
                 , "value": Employee_ID
@@ -444,20 +487,24 @@ function updateRole () {
                             let roles = resData;
 
                             // map roles
+                            // map fn values from result-set alias names
                             const roleList = roles.map(({ Role_ID, Role_Name }) => ({
                                 "name": Role_Name
                                 , "value": Role_ID
                             }));
 
+                            // ask for role
                             inq
                                 .prompt([ {
                                     "name": "role_id"
-                                    , "message": "Select the Role you are assigning"
+                                    , "message": "Select the new Role you are assigning"
                                     , "type": "list"
                                     , "choices": roleList
                                 } ])
                                 .then((resData) => {
                                     const newRoleID = resData.role_id;
+
+                                    // dp update
                                     Employee.updateRole(employeeID, newRoleID);
                                 })
                                 .then(() => console.log("You have Updated the Employee's Role.", ""))
@@ -476,11 +523,18 @@ function updateRole () {
 
 
 
+
+
+
 // Exit Application
 function exit () {
     console.log("Employees, Managed.");
     process.exit();
 }
 
-//
+
+
+
+
+
 // #endregion Complete Action List
